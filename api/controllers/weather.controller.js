890cf73277fdbe2b68c4d3ebb27c74c82b32eb03
Navin Kumaran <navinkumaran2004@gmail.com>
2024-10-20 22:22:37 +0530

    Update weather.controller.js

--- api/controllers/weather.controller.js
+++ api/controllers/weather.controller.js
@@ -2,7 +2,7 @@ export const getData = async (req, res) => {
   const {city} = req.params;
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=72d5acd9a409b970697976aea796cc6d`
+      ``
     );
     if (!response.ok) {
       throw new Error("Failed to fetch weather data from OpenWeather API");
